349248394f5338dbf4048d1d02604f13550c55d6
VictorSanchezAlpizar <79665924+VictorSanchezAlpizar@users.noreply.github.com>
2024-06-09 21:13:17 -0600

    Update memory_map.h
    Updating memory map

--- systemc/memory_map.h
+++ systemc/memory_map.h
@@ -3,17 +3,36 @@
 
 // Register Info ------------------------------------------
 #define REG_WIDTH 32
-#define TOTAL_REGS 10 //segun el mapa de registros
+#define TOTAL_REGS 6
 
-// Histogram Equalization ---------------------------------
+// Mod: Distance ------------------------------------------
+
+
+// Mod: Camera --------------------------------------------
+
+
+// Mod: LP ------------------------------------------------
+
+
+// Mod: Storage -------------------------------------------
+
+
+// Mod: Histogram Equalization ----------------------------
 #define IMG_ROWS 323
 #define IMG_COLS 434
 #define IMG_H_LENGTH 256
-// Estadistics --------------------------------------------
-#define HOURS_TO_SAVE_DAY 24
+
+// Mod: Estadistics ---------------------------------------
+#define INDEX_FLAG_SAVE_DATA_HOUR 0
+#define INDEX_HOURS_TO_SAVE_DAY 1
 
 // Registers ----------------------------------------------
 // Necesitamos setear esto segun el Memory MAP
-#define REG_A 0x0
+#define REG_DISTANCE 0x0
+#define REG_CAMARA   0x4
+#define REG_LP       0x8
+#define REG_STORAGE  0xC
+#define REG_HIST     0x10
+#define REG_EST      0x14
 
 #endif
