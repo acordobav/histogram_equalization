6e958ed789d95256f11dbdf9e0b06d43163553d5
jj <jillcarranza@gmail.com>
2024-06-09 22:28:43 -0600

    Update testbench_dist_calc.cpp
    tb updated to test animal counting according to ranges

--- systemc/testbench_dist_calc.cpp
+++ systemc/testbench_dist_calc.cpp
@@ -27,6 +27,11 @@ int sc_main(int argc, char* argv[]) {
   sc_signal<const char*> sens_range;
   sc_signal<bool > trigger;
   sc_signal<bool > echo;
+  sc_signal<sc_int <32> > count_near;
+  sc_signal<sc_int <32> > count_half;
+  sc_signal<sc_int <32> > count_far;
+  
+  
   //sc_signal<bool > ;
   
   bool tmp;
@@ -44,6 +49,10 @@ int sc_main(int argc, char* argv[]) {
   	dist.sens_range(sens_range);
   	dist.trigger(trigger);
   	dist.echo(echo);
+  	dist.count_near(count_near);
+  	dist.count_half(count_half);
+    dist.count_far(count_far);
+  
   	//dist.();
 	
   
@@ -57,6 +66,11 @@ int sc_main(int argc, char* argv[]) {
   sc_trace(wf, sens_active, "sens_active");
   sc_trace(wf, trigger, "trigger");
   sc_trace(wf, echo, "echo");
+  sc_trace(wf, count_near, "count_near");
+  sc_trace(wf, count_half, "count_half");
+  sc_trace(wf, count_far, "count_far");
+  
+  
   
   sc_start();
   cout << "@" << sc_time_stamp()<< endl;   
@@ -71,11 +85,11 @@ int sc_main(int argc, char* argv[]) {
     tmp = trigger.read();
     //cout << "tmp = " << tmp <<endl;
     echo_temp = trigger_check(tmp);
-    echo.write(echo_temp);
+    echo.write(echo_temp); // echo enabled
     if(echo_temp == true) {
       //printf("- - -60us - - - \n");
-      time = 40 + (rand() % (15000 - 40 +1) );  //randomize (40 - 15000)
-      sc_start(time,SC_US);
+      time = 30 + (rand() % (15000 - 30 +1) );  //randomize (30 - 15000) time elapsed according to distance
+      sc_start(time,SC_US); // wait that time to represent distance t2 - t1
     }
     if(sc_time_stamp().to_seconds() >= 1) {
       break;
@@ -91,10 +105,9 @@ int sc_main(int argc, char* argv[]) {
   cout << "dist_cm tb = " << dist_cm.read() <<endl;
   cout << "sens_range tb = " << sens_range.read() <<endl;
   cout << "sens_active tb = " << sens_active.read() <<endl;
-
-  
-  
-  
+  cout << "count_near tb = " << count_near.read() <<endl;
+  cout << "count_half tb = " << count_half.read() <<endl;
+  cout << "count_far tb = " << count_far.read() <<endl;
   
   cout << "@" << sc_time_stamp() <<" Terminating simulation\n" << endl;
   sc_close_vcd_trace_file(wf);
