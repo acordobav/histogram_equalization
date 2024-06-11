a931087efdcc120c88e16c53a1bc2d8dae1ff7d0
VictorSanchezAlpizar <79665924+VictorSanchezAlpizar@users.noreply.github.com>
2024-06-11 00:09:44 -0600

    Add files via upload
    Updating memory_map per changes during meeting

--- systemc/memory_map.h
+++ systemc/memory_map.h
@@ -2,37 +2,44 @@
 #define _MEMORY_MAP_H
 
 // Register Info ------------------------------------------
-#define REG_WIDTH 32
-#define TOTAL_REGS 6
+#define REG_WIDTH                  32
+#define REG_BYTES                  4
+#define TOTAL_REGS                 18 //8*sensor+8*camera+Histogram+Stadistics
 
 // Mod: Distance ------------------------------------------
-
+#define MASK_DISTANCE              0xFFFF
+#define MASK_IS_ACTIVE             0x10000
+#define MASK_RANGE                 0x60000
 
 // Mod: Camera --------------------------------------------
-
-
-// Mod: LP ------------------------------------------------
-
-
-// Mod: Storage -------------------------------------------
-
+#define MASK_IDENTIFIER            0xFFFF
+#define MASK_CAPTURE               0x10000
 
 // Mod: Histogram Equalization ----------------------------
-#define IMG_ROWS 323
-#define IMG_COLS 434
-#define IMG_H_LENGTH 256
+#define MASK_IMAGE_ROWS           0xFFFF
+#define MASK_IMAGE_COLS           0xFFFF0000
+
+#define IMG_ROWS 323 //define provisional, despues manejado por registro
+#define IMG_COLS 434 //define provisional, despues manejado por registro
+#define IMG_H_LENGTH 256 //define provisional, despues manejado por registro
 
 // Mod: Estadistics ---------------------------------------
-#define INDEX_FLAG_SAVE_DATA_HOUR 0
-#define INDEX_HOURS_TO_SAVE_DAY 1
+#define MASK_FLAG_SAVE_DATA_HOUR  0x0
+#define MASK_HOURS_TO_SAVE_DAY    0x3FE
+#define MASK_RESET                0x400
+
+#define HOURS_TO_SAVE_DAY          24 //define provisional, despues manejado por registro
 
 // Registers ----------------------------------------------
-// Necesitamos setear esto segun el Memory MAP
-#define REG_DISTANCE 0x0
-#define REG_CAMARA   0x4
-#define REG_LP       0x8
-#define REG_STORAGE  0xC
-#define REG_HIST     0x10
-#define REG_EST      0x14
+//Considering 1 register per sensor (8 sensors max)
+#define REG_BASE_1                 0x10000
+#define REG_SENSOR                 0x4
+
+//Considering 1 register per camera (8 cameras max)
+#define REG_BASE_2                 0x10032
+#define REG_CAMARA                 0x4
+
+#define REG_HISTOGRAM              0x10064
+#define REG_STADISTICS             0x10068
 
 #endif
