9fd07f4a05418d908d63499765abcb6de5ba5a35
jj <jillcarranza@gmail.com>
2024-07-14 23:41:20 -0600

    Update camara_sensor.cpp
    Updated

--- systemc/Proyecto_2_3/camara_sensor.cpp
+++ systemc/Proyecto_2_3/camara_sensor.cpp
@@ -14,13 +14,19 @@ unsigned int prueba[420546];
 unsigned int analog_prueba[420546];
 unsigned int digital_prueba[420546]; 
 uint8_t save_image[ROWS][COLS][3] = {0};
-camara_sensor::camara_sensor(sc_core::sc_module_name cam_nm, double const_digitalization_)
+camara_sensor::camara_sensor(sc_core::sc_module_name nm, double const_digitalization_, double tiempo_)
 :
 signal_in("signal_in"),
+
 //sens_active
 //digital_image_ready
 const_digitalization(const_digitalization_)
 {}
+
+void camara_sensor::set_attributes()
+{
+	tiempo.set_timestep(1000000.0, sc_core::SC_MS);
+}
 
 void camara_sensor::processing()
 {
@@ -31,47 +37,48 @@ void camara_sensor::processing()
   	const char* output_filename = "digital_image.jpg";
   	unsigned char* original_image;
 	digital_image_ready.write(false);
+	//printf("Generando imagen :D"); 
 	//printf("HOla");
-	/*if(signal_in.read()){*/
-	 original_image = stbi_load(input_filename, &original_width, &original_height, &channels, 0);
+	cout << "Signal In " << signal_in<< "\n";  
+	printf("Signal In"); 
+	if(signal_in.read()){
+		printf("Generando imagen :D"); 
+	 	original_image = stbi_load(input_filename, &original_width, &original_height, &channels, 0);
 	//printf("HOla2");
 		
 	//cout << "original_width!!" << original_width << "\n";  
 	//cout << "original_height!!" << original_height << "\n"; 
 	//cout << "channels!!" << channels << "\n"; 
-	for (int y = 0; y < (original_width*original_height*channels); y++) {
+		for (int y = 0; y < (original_width*original_height*channels); y++) {
 	//cout << "HOla3!!" << y << "\n";  
-         analog_prueba[y] = (static_cast<unsigned int>(original_image[y])*190); // representacion de 0 a 256, que representa 0.019, pero 19 para no trabajar con floats
+         	analog_prueba[y] = (static_cast<unsigned int>(original_image[y])*190); // representacion de 0 a 256, que representa 0.019, pero 19 para no trabajar con floats
          //cout << "Listen!!" << y << "\n";  
-    }
-    
-    	stbi_write_jpg(output_analog_filename, original_width, original_height, channels, analog_prueba, 100); 
-    	
-	for (int y = 0; y < (original_width*original_height*channels); y++) {
-         digital_prueba[y] = (static_cast<unsigned int>(analog_prueba[y])/190); 
-         //cout << "Listen!!" << y << "\n";  
-    } 
+    		}
+    		stbi_write_jpg(output_analog_filename, original_width, original_height, channels, analog_prueba, 100); // Calidad
 	
-	for (int i=0; i<original_height; i++){
-		for (int j=0; j<original_width; j++){
-			for (int k=0; k<channels; k++){
-				save_image[i][j][k] = digital_prueba[index];
-				index++;
+    		
+		for (int y = 0; y < (original_width*original_height*channels); y++) {
+         	digital_prueba[y] = (static_cast<unsigned int>(analog_prueba[y])/190); 
+         //cout << "Listen!!" << y << "\n";  
+    		} 
+	
+		for (int i=0; i<original_height; i++){
+			for (int j=0; j<original_width; j++){
+				for (int k=0; k<channels; k++){
+					save_image[i][j][k] = digital_prueba[index];
+					index++;
 				//cout << "Listen!!" << index << "\n";  
-			}
-	        }
+				}
+	       	 	}
+		}
+	
+		stbi_write_jpg(output_filename, original_width, original_height, channels, save_image, 100); // Calidad
+	
 	}
-	
-	stbi_write_jpg(output_filename, original_width, original_height, channels, save_image, 100); // Calidad
-	
-		
-	
 	stbi_image_free(original_image);
-  	//stbi_image_free(digital_image);
+  	//stbi_image_free(analog_prueba);
+	//stbi_image_free(digital_prueba);
 	digital_image_ready.write(true);
-
-
-
 }
 
 
