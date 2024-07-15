31fcd4ea35ca2b606c2950c6b769691b6e84b66e
jj <jillcarranza@gmail.com>
2024-07-14 23:41:54 -0600

    Update camara_sensor.h
    Updated

--- systemc/Proyecto_2_3/camara_sensor.h
+++ systemc/Proyecto_2_3/camara_sensor.h
@@ -10,15 +10,18 @@
 class camara_sensor : public sca_tdf::sca_module
 {
 	public: //signals
-		sca_tdf:: sca_in<bool> signal_in; 
+		sca_tdf:: sc_in<bool> signal_in; 
 		sca_tdf:: sca_out<bool> digital_image_ready;
 
-		camara_sensor(sc_core::sc_module_name cam_nm, double const_digitalization);
+		camara_sensor(sc_core::sc_module_name nm, double const_digitalization, double tiempo);
 		
 	void processing();
 	
+	void set_attributes();
+
 	private: 
 		double const_digitalization;
+		double tiempo; 
 		/*unsigned int prueba[420546];
 		unsigned int analog_prueba[420546];
 		unsigned int digital_prueba[420546]; 
