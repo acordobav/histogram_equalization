b122709f0c08d593ddec6ec0acc603829859643f
jj <jillcarranza@gmail.com>
2024-07-20 19:08:40 -0600

    Update camara_sensor_TLM.cpp

--- systemc/Proyecto_2_3/camara_sensor_TLM.cpp
+++ systemc/Proyecto_2_3/camara_sensor_TLM.cpp
@@ -1,7 +1,12 @@
 //#include "utils.hpp"
 #include "memory_map.h"
-#include "camara_sensor.cpp"
-
+#include "global_register_bank.hpp"
+#include <cstdlib> 
+//#include "calc_dist_TLM.cpp"
+//#include <algorithm>
+//#include "camara_sensor.h"
+//#include "systemc.h"
+//#include "systemc-ams.h"
 class signal_driver2 : public sca_tdf::sca_module {
   public:
       sca_tdf::sca_out<bool> out;
@@ -44,17 +49,31 @@ struct CamaraSensTLM: sc_module
   tlm::tlm_phase phase_pending; 
   sc_time delay_pending;
 
-  // Internal data buffer used by initiator with generic payload  
+  // Internal data buffer used by initiator with generic payload
+   
   camara_sensor* cs1;
   signal_driver2* sig_drv;
+  //CalcDistTLM* calc_dist_TLM_Mod;  //Instanciando modulo 
+  //dist_calc* calcDistMod;
+  
+  sc_signal<bool> sens_active;
   //using namespace sc_core;
-	double const_digitalization = 190;
-	double tiempo;
-  sca_tdf::sca_signal<bool> sens_active_cable;
+  double const_digitalization = 190;
+  double tiempo;
+  //sc_signal<bool> sens_active;
+  
+  sc_signal<bool> sens_active_in;
   sca_tdf::sca_signal<double> echo_signal_cable;
-	sca_tdf::sca_signal<double> time_signal_cable;
-	sca_tdf::sca_signal<bool> image_ready_cable;
+  sca_tdf::sca_signal<double> time_signal_cable;
+  sc_signal<bool> digital_image_ready;
   int data;
+  int tmp;
+  
+  //bool image_ready_output; 
+  //bool sens_active_output;
+  bool sens_active_result;
+  bool digital_image_result;
+  //int digital_image_result;
 
   SC_CTOR(CamaraSensTLM)   
   : target_socket("CamaraSensTLM:target"),
@@ -63,18 +82,19 @@ struct CamaraSensTLM: sc_module
     // Register callbacks for incoming interface method calls
     target_socket.register_nb_transport_fw(this, &CamaraSensTLM::target_nb_transport_fw);
     initiator_socket.register_nb_transport_bw(this, &CamaraSensTLM::initiator_nb_transport_bw);
-
-    /* 
-    //Inst new CAMARA SENSOR
+    
+    //calc_dist_TLM_Mod = new CalcDistTLM("cdTLMM");
+    //calc_dist_TLM_Mod->sens_active(sens_active);
+    //calcDistMod = new dist_calc("cdM");
+    //calcDistMod->sens_active(sens_active);
+    
+    
+    
     cs1 = new camara_sensor("cs1", const_digitalization, tiempo);
-    sig_drv = new signal_driver2("sig_drv");
-
-    cs1->signal_in(sens_active_cable);
-    sig_drv->out(sens_active_cable); //Signals are going to be 'connected', to produce the required behavior
-
-    cs1->digital_image_ready(image_ready_cable);
-    */
-
+    cs1->sens_active_in(sens_active_in);
+    cs1->digital_image_ready(digital_image_ready);
+ 
+    
     SC_THREAD(thread_process);   
   }
 
@@ -85,7 +105,7 @@ struct CamaraSensTLM: sc_module
     while(true){
         // Wait for an event to pop out of the back end of the queue   
         wait(e1);
-
+        //tlm::tlm_generic_payload trans;
         ID_extension* id_extension = new ID_extension;
         trans_pending->get_extension(id_extension); 
 
@@ -95,101 +115,75 @@ struct CamaraSensTLM: sc_module
         cout << name() << " BEGIN_RESP SENT" << " TRANS ID " << id_extension->transaction_id <<  " at time " << sc_time_stamp() << endl;
         
         //Variables to store the incoming data
-        bool sens_active = false;
+        	//sens_active_result = false;
+ 			//cout << "HI P E O P L E     " << endl;
+        	  //if (global_register_bank.read_bits(REG_BASE_2+0x2,0x1)){
+            	unsigned char* data_ptr = trans_pending->get_data_ptr();
+            	memcpy(&tmp, data_ptr, sizeof(int));
+               //cout << "HI P E O P L E     " << tmp << endl;
+            	sens_active_in.write(true);
+            	wait(1000, SC_US);
+            	
+  
+ 		 	digital_image_result = digital_image_ready.read();
+ 		 	//wait(10, SC_US);
+ 		 	//cout << "HI P E O P L E     " << digital_image_result << endl;
+           	if( digital_image_result){
+            
+            		global_register_bank.write_bits(REG_BASE_3+0x2,0x1,0x1);
+            		wait(20, SC_US);
+                    //cout << "Será que no lo escribe....     " << digital_image_result << endl;
+            	}
+            	//global_register_bank.write_bits(REG_BASE_2+0x2,0x1,0x0);
+			if (global_register_bank.read_bits(REG_BASE_3+0x2,0x1)){ 
+			
+			    
+			  }        
+            
+            
+            //}
+            //break;
+          
+            tlm::tlm_phase phase = tlm::BEGIN_RESP;
+            
+        	  //status = target_socket->nb_transport_bw(*trans_pending, phase, delay_pending);
+           
+           // if (status != tlm::TLM_ACCEPTED) {
+		  //  cout << name() << " unknown response TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
+            //  }
+          // TLM2 generic payload transaction
+        	tlm::tlm_generic_payload trans;
+        	id_extension = new ID_extension;
+        	trans.set_extension( id_extension ); // Add the extension to the transaction
 
-        if (global_register_bank.read_bits(REG_BASE_2+0x2,0x1)){
-            unsigned char* data_ptr = trans_pending->get_data_ptr();
-            memcpy(&sens_active, data_ptr, sizeof(bool));
+        	id_extension->transaction_id = generateUniqueID();
+        	
 
-            cout << "From TARGET CAM SENSOR" << endl;
-            cout << "Sens active bool coming from sensor : " << sens_active << endl;
+        	phase = tlm::BEGIN_REQ;   
+          sc_time delay = sc_time(10, SC_NS); 
+            	
 
-            //cout << "FROM REGISTER STATUS OF SENS_ACTIVE: " << global_register_bank.read_bits(REG_BASE_2+0x2,0x1) << endl;
+         digital_image_result=1; trans.set_data_length( sizeof(digital_image_result) );
+	    	unsigned char data2[sizeof(int)]; //Here it is going to be concatenated the data to be sent
+          memcpy(data2, &digital_image_result, sizeof(int));
+          trans.set_data_ptr( data2 ); 
+            	
+         // cout << "DATA output file digital: " << digital_image_result << endl;
+	  
+ 		cout << name() << " BEGIN_REQ SENT" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
+		status = initiator_socket->nb_transport_fw( trans, phase, delay );  // Non-blocking transport call   
+	    
+		// Check value returned from nb_transport   
+		if (status != tlm::TLM_ACCEPTED) {
+		    cout << name() << " unknown response TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
+		    }
+		}
+        
 
-            //Since camara sensor was actived, now disabling it for next iteration
-            global_register_bank.write_bits(REG_BASE_2+0x2,0x1,0x0);
-        }
-
-        tlm::tlm_phase phase = tlm::BEGIN_RESP;
-        status = target_socket->nb_transport_bw(*trans_pending, phase, delay_pending);
-
-        // Check value returned from nb_transport   
-        if (status != tlm::TLM_ACCEPTED) {
-        cout << name() << " unknown response TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-        }
-
-        /*
-        uint8_t** image = createMatrix(ROWS, COLS);
-
-        // Process DATA coming from Sensor
-        if (sens_active){
-            // Load image
-            int width, height, channels;
-            uint8_t* img = stbi_load("../Proyecto_1/Module_Equalization/sydney.jpg", &width, &height, &channels, 0);
-
-            // Extract the red channel
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    int index = (y * width + x) * channels;
-                    image[y][x] = img[index]; // Red channel is the first byte in each pixel
-                }
-            }
-
-            // Free the image memory
-            stbi_image_free(img);
-        }
-        */
-
-        uint8_t** image = createMatrix(ROWS, COLS);
-
-        // Load image
-        int width, height, channels;
-        uint8_t* img = stbi_load("../Proyecto_1/Module_Equalization/sydney.jpg", &width, &height, &channels, 0);
-
-        // Extract the red channel
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int index = (y * width + x) * channels;
-                image[y][x] = img[index]; // Red channel is the first byte in each pixel
-            }
-        }
-
-        // Free the image memory
-        stbi_image_free(img);
-
-        // TLM2 generic payload transaction
-        tlm::tlm_generic_payload trans;
-        id_extension = new ID_extension;
-        trans.set_extension( id_extension ); // Add the extension to the transaction
-
-        id_extension->transaction_id = generateUniqueID();
-
-        phase = tlm::BEGIN_REQ;   
-        sc_time delay = sc_time(10, SC_NS);   
-
-        //Data to be sent        
-        phase = tlm::BEGIN_REQ;   
-        delay = sc_time(10, SC_NS);   
-                
-        tlm::tlm_command cmd = static_cast<tlm::tlm_command>(rand() % 2);   
-        trans.set_data_ptr( reinterpret_cast<unsigned char*>(image) );   
-        trans.set_data_length( sizeof(image) );   
-
-        // Other fields default: byte enable = 0, streaming width = 0, DMI_hint = false, no extensions   
-        wait( sc_time(10, SC_NS) );
-        tlm::tlm_sync_enum status; 
-
-        cout << name() << " BEGIN_REQ SENT" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-        status = initiator_socket->nb_transport_fw( trans, phase, delay );  // Non-blocking transport call   
-    
-        // Check value returned from nb_transport   
-        if (status != tlm::TLM_ACCEPTED) {
-            cout << name() << " unknown response TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-        }
+        
     }   
-  }
-
-
+		
+  
   // TLM-2 non-blocking transport method
   virtual tlm::tlm_sync_enum target_nb_transport_fw(
     tlm::tlm_generic_payload& trans,
@@ -239,7 +233,6 @@ struct CamaraSensTLM: sc_module
     // Initiator obliged to check response status   
     if (trans.is_response_error() )   
       SC_REPORT_ERROR("TLM2", "Response error from nb_transport");   
-          
     //Delay
     wait(delay);
     
@@ -247,4 +240,5 @@ struct CamaraSensTLM: sc_module
     
     return tlm::TLM_ACCEPTED;   
   }
+  
 };
