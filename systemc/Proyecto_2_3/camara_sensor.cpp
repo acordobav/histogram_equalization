cafbbbe7b731d6b881bdfadb9a7724ae59903b4d
jj <jillcarranza@gmail.com>
2024-07-22 23:45:31 -0600

    Update camara_sensor.cpp
    Borrando comentarios innecesarios.

--- systemc/Proyecto_2_3/camara_sensor.cpp
+++ systemc/Proyecto_2_3/camara_sensor.cpp
@@ -1,9 +1,6 @@
 #include <systemc.h>
 #include "camara_sensor.h"
 #include <systemc-ams.h>
-//#include "utils.hpp"
-//#include "camara_sensor_TLM.cpp"
-//#include "camara_source.cpp"
 
 #define ROWS 323
 #define COLS 434
@@ -18,8 +15,6 @@ camara_sensor::camara_sensor(sc_core::sc_module_name nm, double const_digitaliza
 :
 sens_active_in("sens_active_in"),
 digital_image_ready("digital_image_ready"),
-//sens_active
-//digital_image_ready
 const_digitalization(const_digitalization_)
 {}
 
@@ -53,34 +48,19 @@ void camara_sensor::processing()
   	else{
   		timer++; 
   	}
-  	//printf("Signal In"); 
-  	//bool verificacion = signal_in.read();
-  	
-	//digital_image_ready.write(false);
-	//printf("Generando imagen :D"); 
-	//printf("HOla");
-	//cout << "COUNT " << count << "\n";  
-	
-	//count++;
-	//printf("Signal In in camara sensor"); 
-	//cout << "channels!!" << sens_active_in.read() << "\n"; 
+
 	if(sens_active_in.read()){
-	//}*/
-		//printf("Generando imagen :D"); 
 	 	original_image = stbi_load(input_filename, &original_width, &original_height, &channels, 0);
 
-	//cout << "channels!!" << sens_active_in.read() << "\n"; 
-		for (int y = 0; y < (original_width*original_height*channels); y++) {
-	//cout << "HOla3!!" << y << "\n";  
-         	analog_prueba[y] = (static_cast<unsigned int>(original_image[y])*190); // representacion de 0 a 256, que representa 0.019, pero 19 para no trabajar con floats
+		for (int y = 0; y < (original_width*original_height*channels); y++) { 
+         	analog_prueba[y] = (static_cast<unsigned int>(original_image[y])*120); // representacion de 0 a 256, que representa 0.019, pero 19 para no trabajar con floats
          
     		}
     		stbi_write_jpg(output_analog_filename, original_width, original_height, channels, analog_prueba, 100); // Calidad
 	
     		
 		for (int y = 0; y < (original_width*original_height*channels); y++) {
-         	digital_prueba[y] = (static_cast<unsigned int>(analog_prueba[y])/190); 
-         //cout << "Listen!!" << y << "\n";  
+         	digital_prueba[y] = (static_cast<unsigned int>(analog_prueba[y])/120);  
     		} 
 	
 		for (int i=0; i<original_height; i++){
@@ -95,24 +75,9 @@ void camara_sensor::processing()
 	
 		stbi_write_jpg(output_filename, original_width, original_height, channels, save_image, 100); // Calidad
 	
-	stbi_image_free(original_image);
-	//free (original_image);
-	//free(save_image);
-  	//stbi_image_free(analog_prueba);
-	//stbi_image_free(digital_prueba);
-	
+	stbi_image_free(original_image);	
 	digital_image_ready.write(true);
-	//cout << "channels!!" << sens_active_in.read() << "\n"; 
 	}
-	//printf("Final :D");
-	
-/*	
-bool read_image_ready(){    //return value 
-  	return digital_image_ready;
-}
-*/
-
-
 }
 
 
